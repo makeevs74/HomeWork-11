46dd643421ec070cd203fa99206d236a7fbe4266
makeevs74 <makeevs74@gmail.com>
2022-08-18 14:17:49 +0500

    Add buttons, labels and textField

--- HomeWork-11/ViewController.swift
+++ HomeWork-11/ViewController.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var labelOrConnectWith: UILabel!
+    @IBOutlet weak var textFieldLogin: UIImageView!
+    @IBOutlet weak var textFieldPassword: UITextField!
+    @IBOutlet weak var buttonLogin: UIButton!
+    @IBOutlet weak var buttonFacebook: UIButton!
+    @IBOutlet weak var buttonTwitter: UIButton!
+    @IBOutlet weak var buttonSignUp: UIButton!
+    @IBOutlet weak var viewWallpaper: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
